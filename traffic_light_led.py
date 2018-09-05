f1fbe9ef2b9d46298a21a9cabe76ea1731a341a5
Abdul Azeez <azeezshahrizal@gmail.com>
2018-09-05 18:10:45 +0800

    Create traffic_light_led.py

---
+++ traffic_light_led.py
@@ -0,0 +1,58 @@
+# Ensure that you run this script via local machine Python 3
+# This script assumes you use Pin 14 for Button
+# This script assumes you use Pin 15 for Green LED
+# This script assumes you use Pin 18 for Yellow LED
+# This script assumes you use Pin 23 for Red LED
+# This script assumes you use Pin 24 for Green LED Blinking
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
+input_state = GPIO.input(18)
+
+while True:
+  
+  if input_state == False:
+    print("Button Pressed")
+    time.sleep(0.2)
+    
+    print("LED ON")
+    GPIO.output(15, GPIO.HIGH)
+    time.sleep(0.5)
+
+    print("LED OFF")
+    GPIO.output(15, GPIO.LOW)
+    time.sleep(0.5)
+    
+    print("LED ON")
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(0.5)
+
+    print("LED OFF")
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(0.5)
+    
+    print("LED ON")
+    GPIO.output(23, GPIO.HIGH)
+    time.sleep(0.5)
+
+    print("LED OFF")
+    GPIO.output(23, GPIO.LOW)
+    time.sleep(0.5)
+    
+    for x in range(0, 5):
+      print("LED ON")
+      GPIO.output(24, GPIO.HIGH)
+      time.sleep(0.5)
+
+      print("LED OFF")
+      GPIO.output(24, GPIO.LOW)
+      time.sleep(0.5)
