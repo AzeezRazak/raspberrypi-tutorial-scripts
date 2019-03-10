1c2cc9e152a0b12cd51c7b8dd23fbdedc62f23c6
Abdul Azeez <azeezshahrizal@gmail.com>
2019-03-10 14:27:41 +0800

    Update traffic_light_led.py

--- traffic_light_led.py
+++ traffic_light_led.py
@@ -16,10 +16,12 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 
-input_state = GPIO.input(18)
+
 
 while True:
   
+  
+  input_state = GPIO.input(18)
   if input_state == False:
     print("Button Pressed")
     time.sleep(0.2)
